6f2d931118c383e6179996c22d9760d77b13d6b0
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2024-06-13 21:50:39 +0900

    Add order by asc

--- src/main/kotlin/org/example/indiejoaapi/live/repository/LiveRepository.kt
+++ src/main/kotlin/org/example/indiejoaapi/live/repository/LiveRepository.kt
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository
 
 interface LiveRepository : JpaRepository<Live, Long> {
     fun findByIndieStreetId(indieStreetId: Long): Live?
-    fun findByTitleContains(title: String, pageRequest: PageRequest): Page<Live>
+    fun findByTitleContainsOrderByStartDateAsc(title: String, pageRequest: PageRequest): Page<Live>
     fun findByTitleContainsOrderByStartDateDesc(
         title: String,
         pageRequest: PageRequest
